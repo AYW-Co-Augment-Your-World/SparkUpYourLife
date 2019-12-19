a0847f26fe57ac99db00ad34444e1c3db7d29457
jfuller957 <jfuller957@gmail.com>
2019-12-18 18:54:12 -0800

    stylesheet update

--- js/JamesCardMarker.js
+++ js/JamesCardMarker.js
@@ -25,115 +25,112 @@ import {
   ViroQuad
 } from 'react-viro';
 
-
-
 export class JamesCardMarker extends Component {
   state = {
     runAnimation: false
   };
   render() {
     return (
-
       <ViroARImageMarker
-          target={'businessCard'}
-          onAnchorFound={() =>
-            this.setState({
-              runAnimation: true
-            })
-          }
-        >
-          <ViroNode key="card">
-            <ViroNode
-              opacity={0}
-              position={[0, -0.02, 0]}
-              animation={{
-                name: 'animateImage',
-                run: this.state.runAnimation
-              }}
-            >
-              <ViroFlexView
-                rotation={[-90, 0, 0]}
-                height={0.03}
-                width={0.05}
-                style={styles.card}
-              >
-                <ViroFlexView style={styles.cardWrapper}>
-                  <ViroImage
-                    height={0.015}
-                    width={0.015}
-                    style={styles.image}
-                    source={require('./res/avatar.jpeg')}
-                    onClick={() => alert('Contact Saved')}
-                  />
-                  <ViroText
-                    textClipMode="None"
-                    text="James Fuller"
-                    scale={[0.015, 0.015, 0.015]}
-                    style={styles.textStyle}
-                  />
-                </ViroFlexView>
-                <ViroFlexView style={styles.subText}>
-                  <ViroText
-                    textClipMode="None"
-                    text="@jfuller957"
-                    scale={[0.01, 0.01, 0.01]}
-                    style={styles.textStyle}
-                  />
-                  <ViroText
-                    textClipMode="None"
-                    text="#jfuller957"
-                    scale={[0.01, 0.01, 0.01]}
-                    style={styles.textStyle}
-                  />
-                  <ViroFlexView style={styles.iconBar}>
-                    <ViroAnimatedImage
-                      style={styles.image}
-                      loop={true}
-                      source={require('./res/linkedin.gif')}
-                      onClick={() => Linking.openURL('https://www.linkedin.com/in/arthursandro/')}
-                    />
-                    <ViroImage
-                      style={styles.image}
-                      source={require('./res/github.png')}
-                      onClick={() => Linking.openURL('https://github.com/arthurs14')}
-                    />
-                    <ViroImage
-                      style={styles.image}
-                      source={require('./res/message.png')}
-                      onClick={() => alert('message redirection')}
-                    />
-                  </ViroFlexView>
-                </ViroFlexView>
-              </ViroFlexView>
-            </ViroNode>
-            <ViroNode
-              opacity={0}
-              position={[0, 0, 0]}
+        target={'businessCard'}
+        onAnchorFound={() =>
+          this.setState({
+            runAnimation: true
+          })
+        }
+      >
+        <ViroNode key="card">
+          <ViroNode
+            opacity={0}
+            position={[0, -0.02, 0]}
+            animation={{
+              name: 'animateImage',
+              run: this.state.runAnimation
+            }}
+          >
+            <ViroFlexView
               rotation={[-90, 0, 0]}
-              animation={{
-                name: 'animateViro',
-                run: this.state.runAnimation
-              }}
+              height={0.03}
+              width={0.05}
+              style={styles.card}
             >
-              <ViroFlexView flexDirection={'column'}>
+              <ViroFlexView style={styles.cardWrapper}>
+                <ViroImage
+                  height={0.015}
+                  width={0.015}
+                  style={styles.image}
+                  source={require('./res/avatar.jpeg')}
+                  onClick={() => alert('Contact Saved')}
+                />
                 <ViroText
-                  text="www.jfuller957.com"
-                  scale={[0.01, 0.01, 0.01]}
-                  padding={0.001}
+                  textClipMode="None"
+                  text="James Fuller"
+                  scale={[0.015, 0.015, 0.015]}
+                  style={styles.textStyle}
                 />
               </ViroFlexView>
-            </ViroNode>
+              <ViroFlexView style={styles.subText}>
+                <ViroText
+                  textClipMode="None"
+                  text="@jfuller957"
+                  scale={[0.01, 0.01, 0.01]}
+                  style={styles.textStyle}
+                />
+                <ViroText
+                  textClipMode="None"
+                  text="#jfuller957"
+                  scale={[0.01, 0.01, 0.01]}
+                  style={styles.textStyle}
+                />
+                <ViroFlexView style={styles.iconBar}>
+                  <ViroAnimatedImage
+                    style={styles.image}
+                    loop={true}
+                    source={require('./res/linkedin.gif')}
+                    onClick={() =>
+                      Linking.openURL(
+                        'https://www.linkedin.com/in/arthursandro/'
+                      )
+                    }
+                  />
+                  <ViroImage
+                    style={styles.image}
+                    source={require('./res/github.png')}
+                    onClick={() =>
+                      Linking.openURL('https://github.com/arthurs14')
+                    }
+                  />
+                  <ViroImage
+                    style={styles.image}
+                    source={require('./res/message.png')}
+                    onClick={() => alert('message redirection')}
+                  />
+                </ViroFlexView>
+              </ViroFlexView>
+            </ViroFlexView>
           </ViroNode>
-        </ViroARImageMarker>
-
-
-
+          <ViroNode
+            opacity={0}
+            position={[0, 0, 0]}
+            rotation={[-90, 0, 0]}
+            animation={{
+              name: 'animateViro',
+              run: this.state.runAnimation
+            }}
+          >
+            <ViroFlexView flexDirection={'column'}>
+              <ViroText
+                text="www.jfuller957.com"
+                scale={[0.01, 0.01, 0.01]}
+                padding={0.001}
+              />
+            </ViroFlexView>
+          </ViroNode>
+        </ViroNode>
+      </ViroARImageMarker>
     );
   }
-
-
 }
-
 
 let styles = StyleSheet.create({
   textStyle: {
@@ -153,6 +150,19 @@ let styles = StyleSheet.create({
     alignItems: 'flex-start',
     padding: 0.001,
     flex: 0.5
+  },
+  iconBar: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    flex: 0.5
+  },
+  image: {
+    height: 0.01,
+    width: 0.01,
+    marginTop: 0.005,
+    marginLeft: 0.002,
+    marginRight: 0.002
   },
   subText: {
     flexDirection: 'column',
